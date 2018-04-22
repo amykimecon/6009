37f3070738163ebcce10580a4de281fd94a4410a
Amy <kimam@utschools.ca>
2018-04-22 13:46:12 -0400

    lab 8a evaluating with builtins

--- lab8A/lab.py
+++ lab8A/lab.py
@@ -87,9 +87,23 @@ def parse(tokens):
         raise SyntaxError
     return parse_expression(0)[0]
 
+def mult(args):
+    prod = 1
+    for elem in args:
+        prod *= elem
+    return prod
+
+def div(args):
+    divided = args[0]
+    for elem in range(1,len(args)):
+        divided = divided/args[elem]
+    return divided
+
 carlae_builtins = {
     '+': sum,
     '-': lambda args: -args[0] if len(args) == 1 else (args[0] - sum(args[1:])),
+    '*': mult,
+    '/': lambda args: args[0] if len(args) == 1 else (args[0]/mult(args[1:]))
 }
 
 def evaluate(tree):
@@ -101,10 +115,34 @@ def evaluate(tree):
         tree (type varies): a fully parsed expression, as the output from the
                             parse function
     """
-    raise NotImplementedError
+    if not isinstance(tree,list):
+        if tree in carlae_builtins:
+            return carlae_builtins[tree]
 
+        if isinstance(tree, float):
+            return tree
+
+    if tree[0] in carlae_builtins:
+        op = carlae_builtins[tree[0]]
+        tree = tree[1:]
+        new_tree = []
+        for subelem in tree:
+            new_tree.append(evaluate(subelem))
+        return op(new_tree)
+
+    else:
+        raise EvaluationError
+
+#print(evaluate(['+', 2.0, ['-', 5.0, 3.0], 7.0, 8.0]))
 
 if __name__ == '__main__':
     # code in this block will only be executed if lab.py is the main file being
     # run (not when this module is imported)
-    pass
+    #pass
+    expr = input(">>> ")
+    while expr != "QUIT":
+        try:
+            print(evaluate(parse(tokenize(expr))))
+        except:
+            print("error, try again")
+        expr = input(">>> ")
